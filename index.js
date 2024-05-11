6ddc970ba58666f0f676757370d7b990ef00d2ab
imxalias123 <sailaxmikomirineni@gmail.com>
2024-05-11 22:49:51 +0530

    Responsive

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let container = document.getElementById("containerId")
 let toggle = document.getElementById("switch");
 let popupElement = document.getElementById("popupId");
+let navigationEle = document.getElementById("navigation_menuId")
 
 function on_off(){
     container.classList.toggle("light_mode");
@@ -14,4 +15,9 @@ function login(){
 
 function close_login_form(){
     popupElement.classList.remove("display")
+}
+
+function navgation_list(){
+    navigationEle.classList.toggle("header_dropdown_container")
+    navigationEle.classList.toggle("header_dropdown_remove")
 }
